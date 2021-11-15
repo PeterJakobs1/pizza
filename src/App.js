565d5535bd687447c82a8f0f865c087c10821a3d
MarcusKri <marcus-kristoffersen@outlook.com>
2021-11-15 16:08:29 +0100

    hoe

--- src/App.js
+++ src/App.js
@@ -1,6 +1,8 @@
 import './App.scss';
 import NavBar from './Components/NavBar';
 import LandingPage from './Pages/LandingPage';
+import ProductPage from './Pages/ProductPage';
+import {Switch, Route} from 'react-router-dom';
 
 function App() {
   return (
@@ -11,6 +13,18 @@ function App() {
       <div className="main-content">
           <div className="content">
             <LandingPage />
+          </div>
+      </div>
+      <div className="main-content">
+          <div className="content">
+            <Switch>
+              <Route path ="/" exact >
+                <ProductPage />
+                </Route>
+                <Route path ="/ProductPage" exact >
+                <ProductPage />
+                </Route>
+            </Switch>
           </div>
       </div>
 
---
+++ src/Pages/ProductPage.js
@@ -0,0 +1,18 @@
+import React from 'react'
+
+function ProductPage() {
+    return (
+        <div className="ProductPage">
+            <header className="ProductText">
+                <h1 className="product-text">
+                    
+                    <h3>Delicious</h3>
+                    <h3>Authentic</h3>
+                    <h3>Italian food</h3>
+                </h1>
+            </header>
+        </div>
+    )
+}
+
+export default ProductPage
