14afa1365e9bd7d559ba7538dd8f691e6ff042a2
MarcusKri <marcus-kristoffersen@outlook.com>
2021-11-16 20:02:21 +0100

    n

--- src/App.js
+++ src/App.js
@@ -1,8 +1,13 @@
 import './App.scss';
 import NavBar from './Components/NavBar';
 import LandingPage from './Pages/LandingPage';
+<<<<<<< HEAD
+import ProductPage from './Pages/ProductPage';
+import {Switch, Route} from 'react-router-dom';
+=======
 import CartPage from './Pages/CartPage';
 
+>>>>>>> 9ea32332e15f53990e4f84a9fc5972895792aa77
 
 function App() {
   return (
@@ -15,8 +20,22 @@ function App() {
             <LandingPage />
           </div>
       </div>
+<<<<<<< HEAD
+      <div className="main-content">
+          <div className="content">
+            <Switch>
+              <Route path ="/" exact >
+                <ProductPage />
+                </Route>
+                <Route path ="/ProductPage" exact >
+                <ProductPage />
+                </Route>
+            </Switch>
+          </div>
+=======
       <div>
       <CartPage/>
+>>>>>>> 9ea32332e15f53990e4f84a9fc5972895792aa77
       </div>
 
     </div>
