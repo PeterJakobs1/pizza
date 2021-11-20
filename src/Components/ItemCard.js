51dc5a4dc05f09cbb6aa048382a6274520680cd3
MarcusKri <marcus-kristoffersen@outlook.com>
2021-11-20 18:03:13 +0100

    k

--- src/Components/ItemCard.js
+++ src/Components/ItemCard.js
@@ -1,16 +1,26 @@
 import React from 'react';
 
 export default class ItemCard extends React.Component {
+
+    constructor(props){
+        super(props);
+    }
+
+    addItem = (name, price) => {
+        alert(name + ' ' + price)
+    }
+
     render() {
         return (
             //TODO STYLE KORT
-            <div className="itemCardContainer">
+            <div className="itemCardContent">
                 {/* PICTURE legge til src*/}
-                <img src={this.props.image} className="itemCardImage" alt="itemCardImage"></img>
+                <img className="itemCardImage" src={this.props.image} alt="itemCardImage"></img>
                 {/* HEADER */}
                 <div className="itemCardName">{this.props.name}</div>
                 {/* PRICE */}
-                <div className="ItemCardPrice">{this.props.price}</div>
+                <div className="itemCardPrice">{this.props.price}</div>
+                <button className="itemCardButton" onClick={() => this.addItem(this.props.name, this.props.price)}>Add pizza</button>
             </div>
         )
     }
