302a56c2b56a99b5c8795e4319b3dd5622617eb9
MarcusKri <marcus-kristoffersen@outlook.com>
2021-11-23 14:05:52 +0100

    navbar fix

--- src/Components/NavBar.js
+++ src/Components/NavBar.js
@@ -1,5 +1,9 @@
 import React from 'react'
 import logo from '../img/logo.jpg'
+import twitterLogo from '../img/twitterLogo.png'
+import facebookLogo from '../img/facebookLogo.png'
+import instagramLogo from '../img/instagramLogo.png'
+import linkedinLogo from '../img/linkedinLogo.png'
 import bergen from '../img/bergen.png'
 import oslo from '../img/oslo.png'
 import { NavLink } from 'react-router-dom';
@@ -11,7 +15,6 @@ function NavBar() {
             <nav className="nav">
                 <div className="logo">
                     <img src={logo} alt="" />
-
                 </div>
                 <ul className="nav-items">
                     <h5 className="nav-item">
@@ -30,7 +33,6 @@ function NavBar() {
                         </NavLink>
                     </h5>
                 </ul>
-
                 <footer className="footer">
                     <div className="location-img-container">
                         <img src={bergen} alt="bergen" />
@@ -40,9 +42,14 @@ function NavBar() {
                         <img src={oslo} alt="oslo" />
                         <h4>Oslo</h4>
                     </div>
+                    <div className="linkContainer">
+                        <img className="logoImage twitterLogo" src={twitterLogo} alt="" />
+                        <img className="logoImage facebookLogo" src={facebookLogo} alt="" />
+                        <img className="logoImage instagramLogo" src={instagramLogo} alt="" />
+                        <img className="logoImage linkedInLogo" src={linkedinLogo} alt="" />
+                    </div>
                 </footer>
             </nav>
-
         </div>
     )
 }
